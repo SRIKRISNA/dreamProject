56ca5b9d734193ce5970799a4d5e90d0cbdd324c
Srikrishna <krishna333sp@gmail.com>
2022-12-31 23:45:20 +0530

    logout implemented

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard(){
+
+    let Navigate = useNavigate();
+    const LogOutHandler = (e) => {
+        localStorage.setItem('authorization', '');
+        Navigate('/');
+    }
     return(
         <>
             <h1>Hello Dash</h1>
-            <div className='logout'><Link to='/'><button id='b-logout' type="">LOGOUT</button></Link></div>
+            <div className='logout'><button id='b-logout' type="submit" onClick={(e)=> LogOutHandler(e)} >LOGOUT</button></div>
         </>
     )
 }
